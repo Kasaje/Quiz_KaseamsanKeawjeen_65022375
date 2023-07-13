bc89931136aad572493615841666cca39088ecea
Kasaje <bowieparkour@gmail.com>
2023-07-13 09:22:29 +0700

    Quiz1

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Main{
+    public static void main(String[] args) {
+        Student Kasaje = new Student(123, "Kasaje", 2565, 3.95);
+        Kasaje.ShowDetails();
+        Kasaje.Setting(555, "Kaito", 2566, 4.00);
+        Kasaje.ShowDetails();
+    }
+}
+
+class Student{
+
+    protected int ID;
+    protected String Name;
+    protected int AddmissionYear;
+    protected double GPA;
+
+    Student(int ID , String Name , int AddmissionYear , double GPA){
+        this.ID = ID;
+        this.Name = Name;
+        this.AddmissionYear = AddmissionYear;
+        this.GPA = GPA;
+    }
+
+    public void ShowDetails(){
+        System.out.println("ID : " + this.ID);
+        System.out.println("Name : " + this.Name);
+        System.out.println("Addmission Year : " + this.AddmissionYear);
+        System.out.println("GPA : " + this.GPA);
+    }
+
+    public void Setting(int ID , String Name , int AddmissionYear , double GPA){
+        this.ID = ID;
+        this.Name = Name;
+        this.AddmissionYear = AddmissionYear;
+        this.GPA = GPA; 
+    }
+}
